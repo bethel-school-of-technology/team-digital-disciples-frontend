e0bdda441311ae8f8b798f43ed2fdd72bf49b457
Kaleb Hendricks <kalebhendricks15@gmail.com>
2022-07-25 14:04:40 -0700

    Added necessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GetRequestComponent } from './fetch.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InboxComponent } from './inbox/inbox.component';
+import { PrayerRequestsComponent } from './prayer-requests/prayer-requests.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, GetRequestComponent, LoginComponent, RegistrationComponent, DashboardComponent, InboxComponent, PrayerRequestsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/prayer-requests/prayer-requests.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prayer-requests',
+  templateUrl: './prayer-requests.component.html',
+  styleUrls: ['./prayer-requests.component.css']
+})
+export class PrayerRequestsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
