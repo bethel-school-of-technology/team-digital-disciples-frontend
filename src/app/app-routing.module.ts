4daef4db553c3cf56bb252a1a906ff7d8532b983
Kaleb Hendricks <kalebhendricks15@gmail.com>
2022-07-26 13:05:11 -0700

    Added Routing to the necessary components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+import { RegistrationComponent } from './registration/registration.component';
+import { PrayerRequestsComponent } from './prayer-requests/prayer-requests.component';
+import { LoginComponent } from './login/login.component';
+import { InboxComponent } from './inbox/inbox.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+const routes: Routes = [
+
+  {
+    path: 'registration',
+    component: RegistrationComponent
+  },
+  {
+    path:'prayerrequests',
+    component: PrayerRequestsComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path:'inbox',
+    component:InboxComponent
+  }, 
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), CommonModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
