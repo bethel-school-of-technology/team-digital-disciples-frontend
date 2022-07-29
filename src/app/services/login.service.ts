7308318fb062d7b4a88efaa5851c477599c9fa92
Chris Raines (Laptop) <itassistcr@gmail.com>
2022-07-28 22:08:08 -0400

    created login service and login function

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,5 +1,7 @@
+import { baseUrl } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +9,7 @@ import { Injectable } from '@angular/core';
 export class LoginService {
 
   constructor(private http : HttpClient) { }
+  login(data):Observable<any>{
+    return this.http.post(`${baseUrl}users/authenticate`, data);
+  }
 }
