9a4b716de77e6b738c9f08340d8516b5d9e0e952
Chris Raines (Laptop) <itassistcr@gmail.com>
2022-07-30 01:13:18 -0400

    registration form added and working. basic styling started

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../services/login-service.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private loginInfo: LoginService) { }
+  formGroup: FormGroup;
+  ngOnInit() {
+    this.initForm();
   }
-
+  initForm(){
+    this.formGroup = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    })
+  }
+  registerProcess(){
+    if (this.formGroup.valid){
+      console.log(this.formGroup.value);
+      this.loginInfo.register(this.formGroup.value).subscribe(result => {
+          console.log(result);
+      })
+    }
+  }
 }
