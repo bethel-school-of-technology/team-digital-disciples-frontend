b0a4e3ac1784d0b0034509d7a251c11995eb85d5
Elizabeth Hanly <e.hanly@comcast.net>
2022-08-09 13:37:40 -0700

    changed api to pull one user

--- src/app/prayer-requests/prayer-requests.component.ts
+++ src/app/prayer-requests/prayer-requests.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PrayerRequest } from '../models/prayerRequest';
 import { PrayerRequestService } from '../services/prayer-request.service';
+import { User } from '../models/User';
 
 @Component({
   selector: 'app-prayer-requests',
@@ -13,9 +14,13 @@ export class PrayerRequestsComponent implements OnInit {
   listOfRequests: PrayerRequest[];
 
   constructor(private prayerService: PrayerRequestService) { }
-
+  public currentUser: User;
+ 
   ngOnInit(): void {
-    this.prayerService.getPrayerRequests().subscribe(result =>{
+    this.currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    this.prayerService.getMyPR(this.currentUser.id).subscribe(result =>{
+     
+
       this.listOfRequests = result;
     });
   }
