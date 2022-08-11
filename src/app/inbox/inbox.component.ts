42a1f5a2729fbd14c9d7729b88686985382ace94
Christopher Raines <christopher.raines@jessup.edu>
2022-08-11 17:45:46 -0400

    finished inbox

--- src/app/inbox/inbox.component.ts
+++ src/app/inbox/inbox.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/User';
+import { LoginService } from '../services/login-service.service';
+import { PrayerResponseService } from '../services/prayer-response.service';
+import { PrayerResponse } from '../models/prayerResponse';
 
 @Component({
   selector: 'app-inbox',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InboxComponent implements OnInit {
 
-  constructor() { }
+  responseService : PrayerResponseService;
+  constructor(responseService : PrayerResponseService ) { 
+    this.responseService = responseService;
+  }
+  public currentUser = new User();
+  prayerResponses : PrayerResponse[];
+  noResponses = false;
+
 
   ngOnInit(): void {
+      this.currentUser = JSON.parse(localStorage.getItem("currentUser"));
+      this.responseService.getResponses(this.currentUser.id).subscribe(result => {
+        if (result.length < 1){
+          this.noResponses = true;
+          console.log(this.noResponses);
+        }    
+        this.prayerResponses = result;
+        
+      })
   }
 
 }
